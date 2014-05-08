ec70630763cb8647c810fae56981b081289c34f4
Seth Fitzsimmons <seth@mojodna.net>
2014-05-08 11:11:25 -0700

    Gross simplification

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changes
+
+## v0.2.0 - 5/8/14
+
+* Remove `tilelive-http` from dependencies
+* Specify `tilejson` and `tilelive` as peer dependencies
+* Use [tilejson](https://github.com/mapbox/node-tilejson) for the heavy lifting
+
+## v0.1.0 - 4/28/14
+
+* Initial released version
--- index.js
+++ index.js
@@ -3,35 +3,17 @@
 var url = require("url"),
     util = require("util");
 
-var HttpSource = require("tilelive-http"),
-    TileJSON = require("tilejson");
-
-var MapBoxSource = function(uri, callback) {
-  uri = url.parse(uri);
-
-  return new TileJSON(util.format("http://a.tiles.mapbox.com/v3%s.json", uri.path),
-                      function(err, data) {
-    if (err) {
-      return callback(err);
-    }
-
-    this.data = data.data;
-
-    return HttpSource.call(this, this.data.tiles[0], callback);
-  }.bind(this));
-};
-
-util.inherits(MapBoxSource, HttpSource);
-
-MapBoxSource.prototype.getInfo = function(callback) {
-  return callback(null, this.data);
-};
-
-MapBoxSource.registerProtocols = function(tilelive) {
-  tilelive.protocols["mapbox:"] = this;
-};
-
 module.exports = function(tilelive, options) {
+  var MapBoxSource = function(uri, callback) {
+    uri = url.parse(uri);
+
+    return tilelive.load(util.format("tilejson+http://a.tiles.mapbox.com/v3%s.json", uri.path), callback);
+  };
+
+  MapBoxSource.registerProtocols = function(tilelive) {
+    tilelive.protocols["mapbox:"] = this;
+  };
+
   MapBoxSource.registerProtocols(tilelive);
 
   return MapBoxSource;
